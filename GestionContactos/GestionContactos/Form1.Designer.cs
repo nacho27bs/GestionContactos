ee6eeb8f9d75a9d6ec2ad1a934c97137a71802ee
IGNACIO BAEZA SÁNCHEZ <nachobs27@gmail.com>
2025-01-28 17:26:53 +0100

    Creación de botones y vectores

--- GestionContactos/GestionContactos/Form1.Designer.cs
+++ GestionContactos/GestionContactos/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnAñadirContacto = new Button();
+            btnModificarTelefono = new Button();
+            btnEliminarContacto = new Button();
+            btnMostrar = new Button();
+            SuspendLayout();
+            // 
+            // btnAñadirContacto
+            // 
+            btnAñadirContacto.Location = new Point(137, 93);
+            btnAñadirContacto.Name = "btnAñadirContacto";
+            btnAñadirContacto.Size = new Size(151, 54);
+            btnAñadirContacto.TabIndex = 0;
+            btnAñadirContacto.Text = "Añadir Contacto";
+            btnAñadirContacto.UseVisualStyleBackColor = true;
+            btnAñadirContacto.Click += btnAñadirContacto_Click;
+            // 
+            // btnModificarTelefono
+            // 
+            btnModificarTelefono.Location = new Point(334, 93);
+            btnModificarTelefono.Name = "btnModificarTelefono";
+            btnModificarTelefono.Size = new Size(151, 54);
+            btnModificarTelefono.TabIndex = 1;
+            btnModificarTelefono.Text = "Modificar Teléfono";
+            btnModificarTelefono.UseVisualStyleBackColor = true;
+            btnModificarTelefono.Click += btnModificarTelefono_Click;
+            // 
+            // btnEliminarContacto
+            // 
+            btnEliminarContacto.Location = new Point(137, 177);
+            btnEliminarContacto.Name = "btnEliminarContacto";
+            btnEliminarContacto.Size = new Size(151, 54);
+            btnEliminarContacto.TabIndex = 2;
+            btnEliminarContacto.Text = "Eliminar Contacto";
+            btnEliminarContacto.UseVisualStyleBackColor = true;
+            btnEliminarContacto.Click += btnEliminarContacto_Click;
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.Location = new Point(334, 177);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(151, 54);
+            btnMostrar.TabIndex = 3;
+            btnMostrar.Text = "Mostrar Contactos";
+            btnMostrar.UseVisualStyleBackColor = true;
+            btnMostrar.Click += btnMostrar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnMostrar);
+            Controls.Add(btnEliminarContacto);
+            Controls.Add(btnModificarTelefono);
+            Controls.Add(btnAñadirContacto);
+            Name = "Form1";
+            Text = "Gestión Contactos";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnAñadirContacto;
+        private Button btnModificarTelefono;
+        private Button btnEliminarContacto;
+        private Button btnMostrar;
     }
 }
--- GestionContactos/GestionContactos/Form1.cs
+++ GestionContactos/GestionContactos/Form1.cs
@@ -2,9 +2,32 @@ namespace GestionContactos
 {
     public partial class Form1 : Form
     {
+        private const int MAX_CONTACTOS = 10;
+        private string[] nombres = new string[MAX_CONTACTOS];
+        private string[] telefonos = new string[MAX_CONTACTOS];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAñadirContacto_Click(object sender, EventArgs e)
+        {
+            /*Hecho por nacho*/
+        }
+
+        private void btnEliminarContacto_Click(object sender, EventArgs e)
+        {
+            /*Hecho por brayan*/
+        }
+
+        private void btnModificarTelefono_Click(object sender, EventArgs e)
+        {
+            /*Hecho por nacho*/
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            /*Hecho por brayan*/
         }
     }
 }
